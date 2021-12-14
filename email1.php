93b02aa943c6d7d134e0cb9f7fa1aace39f55b7d
Azam Azher <95758504+azamazher@users.noreply.github.com>
2021-12-15 02:59:13 +0530

    Add files via upload

---
+++ email1.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?> <?php echo $_POST["lname"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your Message was about: <?php echo $_POST["Message"]; ?>
+
+</body>
+</html>
